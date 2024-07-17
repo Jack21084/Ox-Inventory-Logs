570cb097ab02220193ec3d73a204cd0810c828af
Jack <142785153+Jack21084@users.noreply.github.com>
2024-07-17 16:32:01 +0500

    Add files via upload

---
+++ configs/sv_logs.lua
@@ -0,0 +1,144 @@
+webhooks = {
+    ['drop'] = '',
+    ['pickup'] = '',
+    ['give'] = '',
+    ['stash'] = '',
+}
+hooks = {
+    ['drop'] = {
+        from = 'player',
+        to = 'drop',
+        callback = function(payload)
+            local playerName = GetPlayerName(payload.source)
+            local playerIdentifier = GetPlayerIdentifiers(payload.source)[1]
+            local playerCoords = GetEntityCoords(GetPlayerPed(payload.source))
+            sendWebhook('drop', {
+                {
+                    title = 'Drop',
+                    description = ('Player **%s** (%s, %s) **droped** item **%s** x%s (metadata: %s) at coordinates %s.')
+                        :format(
+                            playerName,
+                            playerIdentifier,
+                            payload.source,
+                            payload.fromSlot.name,
+                            payload.fromSlot.count,
+                            json.encode(payload.fromSlot.metadata),
+                            ('%s, %s, %s'):format(playerCoords.x, playerCoords.y, playerCoords.z)
+                        ),
+                    color = 0x00ff00
+                }
+            })
+        end
+    },
+    ['pickup'] = {
+        from = 'drop',
+        to = 'player',
+        callback = function(payload)
+            local playerName = GetPlayerName(payload.source)
+            local playerIdentifier = GetPlayerIdentifiers(payload.source)[1]
+            local playerCoords = GetEntityCoords(GetPlayerPed(payload.source))
+            sendWebhook('pickup', {
+                {
+                    title = 'Pickup',
+                    description = ('Player **%s** (%s, %s) **picked** item **%s** x%s (metadata: %s) **from the ground** at coordinates %s.')
+                        :format(
+                            playerName,
+                            playerIdentifier,
+                            payload.source,
+                            payload.fromSlot.name,
+                            payload.fromSlot.count,
+                            json.encode(payload.fromSlot.metadata),
+                            ('%s, %s, %s'):format(playerCoords.x, playerCoords.y, playerCoords.z)
+                        ),
+                    color = 0x00ff00
+                }
+            })
+        end
+    },
+    ['give'] = {
+        from = 'player',
+        to = 'player',
+        callback = function(payload)
+            if payload.fromInventory == payload.toInventory then return end
+            local playerName = GetPlayerName(payload.source)
+            local playerIdentifier = GetPlayerIdentifiers(payload.source)[1]
+            local playerCoords = GetEntityCoords(GetPlayerPed(payload.source))
+            local targetSource = payload.toInventory
+            local targetName = GetPlayerName(targetSource)
+            local targetIdentifier = GetPlayerIdentifiers(targetSource)[1]
+            local targetCoords = GetEntityCoords(GetPlayerPed(targetSource))
+            sendWebhook('give', {
+                {
+                    title = 'passing items between players',
+                    description = ('Player **%s** (%s, %s) **gave** player **%s** (%s, %s) item **%s** x%s (metadata: %s) on coordinates %s and %s.')
+                        :format(
+                            playerName,
+                            playerIdentifier,
+                            payload.source,
+                            targetName,
+                            targetIdentifier,
+                            targetSource,
+                            payload.fromSlot.name,
+                            payload.fromSlot.count,
+                            json.encode(payload.fromSlot.metadata),
+                            ('%s, %s, %s'):format(playerCoords.x, playerCoords.y, playerCoords.z),
+                            ('%s, %s, %s'):format(targetCoords.x, targetCoords.y, targetCoords.z)
+                        ),
+                    color = 0x00ff00
+                }
+            })
+        end
+    },
+    ['stash_pick'] = {
+        from = 'player',
+        to = 'stash',
+        callback = function(payload)
+            local playerName = GetPlayerName(payload.source)
+            local playerIdentifier = GetPlayerIdentifiers(payload.source)[1]
+            local playerCoords = GetEntityCoords(GetPlayerPed(payload.source))
+            sendWebhook('stash', {
+                {
+                    title = 'Sklad',
+                    description = ('Player **%s** (%s, %s) **gave** item **%s** x%s (metadata: %s) **to warehouse %s** at coordinates %s.')
+                        :format(
+                            playerName,
+                            playerIdentifier,
+                            payload.source,
+                            payload.fromSlot.name,
+                            payload.fromSlot.count,
+                            json.encode(payload.fromSlot.metadata),
+                            payload.toInventory,
+                            ('%s, %s, %s'):format(playerCoords.x, playerCoords.y, playerCoords.z)
+                        ),
+                    color = 0x00ff00
+                }
+            })
+        end
+    },
+    ['stash'] = {
+        from = 'stash',
+        to = 'player',
+        callback = function(payload)
+            local playerName = GetPlayerName(payload.source)
+            local playerIdentifier = GetPlayerIdentifiers(payload.source)[1]
+            local playerCoords = GetEntityCoords(GetPlayerPed(payload.source))
+            sendWebhook('stash', {
+                {
+                    title = 'Sklad',
+                    description = ('Player **%s** (%s, %s) **picked** item **%s** x%s (metadata: %s) **from warehouse %s** at coordinates %s.')
+                        :format(
+                            playerName,
+                            playerIdentifier,
+                            payload.source,
+                            payload.fromSlot.name,
+                            payload.fromSlot.count,
+                            json.encode(payload.fromSlot.metadata),
+                            payload.fromInventory,
+                            ('%s, %s, %s'):format(playerCoords.x, playerCoords.y, playerCoords.z)
+                        ),
+                    color = 0x00ff00
+                }
+            })
+        end
+    },
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Jack'
+description 'discord logs for ox_inventory by jack'
+
+server_scripts {
+    'configs/sv_logs.lua',
+    'server/sv_oxhook.lua',
+    'server/sv_hooks.lua'
+}
